56f9c5220d193c9be0776ea1e540a742fe940cb0
ShivaStha <sthashiva111@gmail.com>
2024-11-07 20:38:16 +0545

    updating all

--- index.js
+++ index.js
@@ -32,34 +32,53 @@
     function calculate() {
 
         var from, to;
-        let vl = 0;
-
-        vl = document.getElementById("tmp").value;
-        from = document.getElementById("from").value;
-        to = document.getElementById("to").value;
-        //console.log(from+" ok  "+to);
-        if (from == "c" && to == "k") {
-
-            vl = celsiusToKelvin(parseInt(vl));
-            console.log("value is " + vl);
-        } else if (from == 'k' && to == 'c') {
-            vl = kelvinToCelsius(parseInt(vl));
-            console.log("value is " + vl);
-        } else if (from == 'c' && to == 'f') {
-            vl = celsiusToFahrenheit(parseInt(vl));
-            console.log("value is " + vl);
-        } else if (from == 'f' && to == 'c') {
-            vl = fahrenheitToCelsius(parseInt(vl));
-            document.getElementById("rr").innerText = vl;
-        } else if (from == 'f' && to == 'k') {
-            vl = fahrenheitToKelvin(parseInt(vl));
-        } else if (from = "k" && to == "f") {
-
-            vl = kelvinToFahrenheit(parseInt(vl));
-        } else {
-            console.log('same unit ');
+        let vl = document.getElementById("tmp").value;
+        //console.log("logging....."+isNaN(parseInt(vl)));
+        if(!isNaN(parseInt(vl))){
+            
+            from = document.getElementById("from").value;
+            to = document.getElementById("to").value;
+            //console.log(from+" ok  "+to);
+            if (from == "c" && to == "k") {
+    
+                vl = celsiusToKelvin(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Celsius is ";
+                document.getElementById("rr").innerText = vl+" Kelvin";
+                //console.log("value is " + vl);
+            } else if (from == 'k' && to == 'c') {
+                vl = kelvinToCelsius(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Kelvin is ";
+                document.getElementById("rr").innerText = vl+" Celsius";
+                //console.log("value is " + vl);
+            } else if (from == 'c' && to == 'f') {
+                vl = celsiusToFahrenheit(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Celsius is ";
+                document.getElementById("rr").innerText = vl+" Fahrenheit";
+                //console.log("value is " + vl);
+            } else if (from == 'f' && to == 'c') {
+                vl = fahrenheitToCelsius(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Fahrenheit is ";
+                document.getElementById("rr").innerText = vl+" Celsius";
+            } else if (from == 'f' && to == 'k') {
+                vl = fahrenheitToKelvin(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Fahrenheit is ";
+                document.getElementById("rr").innerText = vl+" Kelvin";
+            } else if (from == 'k' && to == 'f') {
+                vl = kelvinToFahrenheit(parseInt(vl));
+                document.getElementById("ff").innerText = document.getElementById("tmp").value+" Kelvin is ";
+                document.getElementById("rr").innerText = vl+" Fahrenheit";
+            } else {
+                alert("your are choosing same unit.Please change it and try .")
+                console.log('same unit '+from+" "+to);
+            }
+    
+    
+            //document.getElementById("ff").innerText = document.getElementById("tmp").value+" "+from+" is ";
+            document.getElementById("wrn").innerText = "";
+        }else{
+            document.getElementById("wrn").innerText = "Please fill all fields!";
         }
 
 
-        document.getElementById("rr").innerText = vl;
+        
     }
